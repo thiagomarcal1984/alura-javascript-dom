7f62f6b90e5c18513070b135585506a1db59bf3a
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-09-27 14:05:51 -0300

    Alterando o elemento.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 const robotron = document.querySelector("#robotron")
-const subtrair = document.querySelector("#subtrair")
-const somar = document.querySelector("#somar")
-const braco = document.querySelector("#braco")
 
 const controle = document.querySelectorAll(".controle-ajuste")
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent)
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
     })
 })
 
@@ -20,10 +17,12 @@ function dizOi(nome) {
 
 dizOi("Zina");
 
-function manipulaDados(operacao) {
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector(".controle-contador")
+
     if (operacao === "-") {
-        braco.value = parseInt(braco.value) - 1;
+        peca.value = parseInt(peca.value) - 1;
     } else {
-        braco.value = parseInt(braco.value) + 1;
+        peca.value = parseInt(peca.value) + 1;
     }
 }
